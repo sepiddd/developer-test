3bfe26b3cf2b3336e0505930529364f5e302c81e
sepiddd <sepide.khadempir@gmail.com>
2021-06-21 16:43:23 +0430

    show the list of reviews

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,38 +1,93 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { dehydrate } from 'react-query/hydration'
-import { QueryClient } from 'react-query';
-import { fetchReviews, useReviews } from '../hooks/reviews';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { dehydrate } from "react-query/hydration";
+import { QueryClient } from "react-query";
+import { fetchReviews, useReviews } from "../hooks/reviews";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
 
 export default function Reviews() {
   const { data, isLoading } = useReviews();
-  
-  if (isLoading) return (
-    <Typography variant="h4" component="h1" gutterBottom>
+  const classes = useStyles();
+
+  if (isLoading)
+    return (
+      <Typography variant="h4" component="h1" gutterBottom>
         Attends un moment !
-    </Typography>
-  );
+      </Typography>
+    );
 
   return (
-    <>
-      <Typography variant="h4" component="h1" gutterBottom>
-          J'aime beaucoup la nourriture
-      </Typography>
-      <Typography variant="h4" component="h1" gutterBottom>
-        {data ? data[0].name : '🤷'}
-      </Typography>
-    </>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Grid
+          container
+          direction="column"
+          justify="flex-start"
+          alignItems="stretch"
+          spacing={2}
+        >
+          <Typography variant="h4" component="h1" gutterBottom>
+            J'aime beaucoup la nourriture
+          </Typography>
+
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right">Description</TableCell>
+                  <TableCell align="right">Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data?.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.text}</TableCell>
+                    <TableCell align="right">
+                      <Button variant="contained" color="primary">
+                        Edit
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Box>
+    </Container>
   );
 }
 
 export async function getStaticProps() {
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(['review'], async () => fetchReviews());
-  
+  await queryClient.prefetchQuery(["review"], async () => fetchReviews());
+
   return {
     props: {
       reviews: dehydrate(queryClient),
     },
   };
 }
-
