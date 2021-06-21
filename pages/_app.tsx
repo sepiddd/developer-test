de92e87beed3b217e3f4824d1725e81e165e43a5
sepiddd <sepide.khadempir@gmail.com>
2021-06-21 17:49:01 +0430

    fix server side rendering issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import Head from 'next/head';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
+  const [queryClient] = React.useState(() => new QueryClient());
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement!.removeChild(jssStyles);
     }
   }, []);
 
-  const queryClientRef = React.useRef<QueryClient>()
-  if (!queryClientRef.current) {
-    queryClientRef.current = new QueryClient()
-  }
-
   return (
-    <QueryClientProvider client={queryClientRef.current}>
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </React.Fragment>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <React.Fragment>
+          <Head>
+            <title>My page</title>
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width"
+            />
+          </Head>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </React.Fragment>
+      </Hydrate>
     </QueryClientProvider>
   );
 }
